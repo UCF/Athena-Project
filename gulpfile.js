d42658ced45ff0864bf0d08da5b7aac57235b638
Jim Barnes <james.barnes@ucf.edu>
2017-03-13 16:31:39 -0400

    Storing css artifact in the repo now.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-clean-css'),
+    bower = require('gulp-bower'),
+    rename = require('gulp-rename'),
+    scsslint = require('gulp-scss-lint'),
+    autoprefixer = require('gulp-autoprefixer');
+
+var config = {
+    scssPath: './_src',
+    cssPath: './assets'
+};
+
+gulp.task('bower', function() {
+    return bower(); 
+});
+
+gulp.task('scss', ['bower'], function() {
+    gulp.src(config.scssPath + '/base.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(rename('main.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 8'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(config.cssPath));
+});
+
+gulp.task('css', ['scss']);
+
+gulp.task('watch', function() {
+    gulp.watch(config.scssPath + '/*.scss', ['css']);
+});
+
+gulp.task('default', ['bower', 'scss']);
