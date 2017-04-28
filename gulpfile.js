ff6be7e047248628c2466fc5e1e5c9ba772ec528
Jo Dickson <jogreybill@gmail.com>
2017-04-28 10:46:46 -0500

    Removed cloud.typography css key for now; added fallback fonts from Athena

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,35 @@ var gulp = require('gulp'),
 
 var config = {
     scssPath: './_src',
-    cssPath: './assets/css'
+    cssPath: './assets/css',
+    srcFontPath: './bower_components/athena-framework/dist/fonts',
+    distFontPath: './assets/fonts'
 };
 
 gulp.task('bower', function() {
     return bower();
 });
+
+gulp.task('move-components-font-sans-serif', ['bower'], function() {
+  return gulp.src([config.srcFontPath + '/ucf-sans-serif-alt/*'])
+    .pipe(gulp.dest(config.distFontPath + '/ucf-sans-serif-alt'));
+});
+
+gulp.task('move-components-font-condensed', ['bower'], function() {
+  return gulp.src([config.srcFontPath + '/ucf-condensed-alt/*'])
+    .pipe(gulp.dest(config.distFontPath + '/ucf-condensed-alt'));
+});
+
+gulp.task('move-components-font-slab-serif', ['bower'], function() {
+  return gulp.src([config.srcFontPath + '/tulia/*'])
+    .pipe(gulp.dest(config.distFontPath + '/tulia'));
+});
+
+gulp.task('move-components-fonts', [
+  'move-components-font-sans-serif',
+  'move-components-font-condensed',
+  'move-components-font-slab-serif'
+]);
 
 gulp.task('scss', ['bower'], function() {
     gulp.src(config.scssPath + '/base.scss')
@@ -33,4 +56,4 @@ gulp.task('watch', function() {
     gulp.watch(config.scssPath + '/*.scss', ['css']);
 });
 
-gulp.task('default', ['bower', 'scss']);
+gulp.task('default', ['bower', 'move-components-fonts', 'scss']);
